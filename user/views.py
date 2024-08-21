5501d83180e412ab8f59e83152d96d1dd7a19ac3
Bright <134352369+secretstar1@users.noreply.github.com>
2024-08-20 19:30:10 -0700

    Update views.py
    Updating views.py

--- user/views.py
+++ user/views.py
@@ -57,6 +57,7 @@ client = OpenAI(
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
     def post(self,request):
+        
         user=request.data
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
