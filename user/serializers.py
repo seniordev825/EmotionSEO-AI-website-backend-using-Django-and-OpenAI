a98914c4f07515164fec63897624ab744ddb36ce
Bright <134352369+secretstar1@users.noreply.github.com>
2024-08-20 19:39:02 -0700

    Update serializers.py
    Updated

--- user/serializers.py
+++ user/serializers.py
@@ -5,6 +5,7 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 class RegisterSerializer(serializers.ModelSerializer):
+    
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
     class Meta:
         model = User
