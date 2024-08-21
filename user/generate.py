3fff0b675f4844a9f79c503a3dafe27a913ef019
unknown <handsome176743@gmail.com>
2024-08-20 17:22:10 -0700

    first commit

--- user/generate.py
+++ user/generate.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv 
 load_dotenv()
-api_key = os.getenv('OPENAI_KEY')
+#api_key = os.getenv('OPENAI_KEY')
 client = OpenAI(api_key = api_key)
 
 
--- user/views.py
+++ user/views.py
@@ -47,7 +47,7 @@ import json
 import base64
 from dotenv import load_dotenv 
 load_dotenv()
-api_key = os.getenv('OPENAI_KEY')
+#api_key = os.getenv('OPENAI_KEY')
 client = OpenAI(
     api_key = api_key
 )
